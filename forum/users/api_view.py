4e31a6109884d3445d33449097d20fc453077125
Leonid Chelakhov <leonidchelahov@gmail.com>
2024-10-07 13:59:23 +0300

    Set Up JWT: change viewsets

--- forum/users/api_view.py
+++ forum/users/api_view.py
@@ -1,29 +1,31 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, RegisterUserSerializer
+from .permissions import IsOwner
 
-class UserViewSet(viewsets.ModelViewSet):
+
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
-class RegisterViewSet(viewsets.ModelViewSet):
+class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = RegisterUserSerializer
     permission_classes = [AllowAny]  
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        user.set_password(request.data['password'])  
-        user.save()
 
         refresh = RefreshToken.for_user(user)
         response_data = {
--- forum/users/serializers.py
+++ forum/users/serializers.py
@@ -6,22 +6,37 @@ class RoleSerializer(serializers.ModelSerializer):
         model = Role
         fields = ('role_id', 'role_name')
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterUserSerializer(serializers.ModelSerializer):
     roles = RoleSerializer(many=True)
-    password = serializers.CharField(write_only=True)  
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = ('user_id', 'user_name', 'first_name', 'last_name', 'email', 'user_phone', 'roles', 'password', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
 
     def create(self, validated_data):
+        # Вилучаємо дані про ролі та пароль
         roles_data = validated_data.pop('roles', [])
-        password = validated_data.pop('password')  
+        password = validated_data.pop('password')
+        
+       
         user = User.objects.create(**validated_data)
-        user.set_password(password)  
+        user.set_password(password)
+        user.save()  
 
         for role_data in roles_data:
             role_name = role_data.get('role_name')
-            role, created = Role.objects.get_or_create(role_name=role_name)
-            user.roles.add(role)
+            if role_name:  
+                role, created = Role.objects.get_or_create(role_name=role_name)
+                user.roles.add(role)
+        
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    roles = RoleSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = ('user_id', 'user_name', 'first_name', 'last_name', 'email', 'user_phone', 'roles', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
