361eab75ee4e792ce9f12cb364f0ca735a8efda3
Mariana Gogol <marianagogol711@gmail.com>
2024-10-15 23:28:55 +0200

    feat: create Room and Message models

--- forum/communications/models.py
+++ forum/communications/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from mongoengine import EmbeddedDocument, StringField, Document, DateTimeField, ListField, \
+    EmbeddedDocumentField
+
+class User(EmbeddedDocument):
+    user_id: StringField(required=True)
+    username: StringField(required=True)
+
+class Message(EmbeddedDocument):
+    sender = EmbeddedDocumentField(User)
+    message = StringField(required=True)
+
+class Room(Document):
+    id = StringField(primary_key=True)
+    created_at = DateTimeField()
+    messages = ListField(EmbeddedDocumentField(Message)) # [{"sender": {"user_id": 1, "username": "user1"}, "message": "Hello"}, {...}]
+    participants = ListField(EmbeddedDocumentField(User)) # [{"user_id": 1, "username": "user1"}, {"user_id": 2, "username": "user_2"}]
