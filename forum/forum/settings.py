7eb3c712dde646c53cef698a26c1d469a9bfea7e
Mariana Gogol <marianagogol711@gmail.com>
2024-10-18 23:03:20 +0200

    feat: develop room and message endpoints

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
 import os
 from datetime import timedelta
+from pathlib import Path
+
 import mongoengine
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -36,7 +37,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 INSTALLED_APPS = [
     "daphne",
-    "channels",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,9 +56,6 @@ INSTALLED_APPS = [
     'channels',
     'django_extensions',
     'notifications.apps.NotificationsConfig',
-    
-
-
 
 ]
 
@@ -188,7 +185,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
