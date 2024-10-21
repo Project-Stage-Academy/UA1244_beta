91861efb741a064aa2a0572fcbc0a3d57520a190
Leonid Chelakhov <leonidchelahov@gmail.com>
2024-10-21 18:11:14 +0300

    Changed some files

--- forum/users/admin.py
+++ forum/users/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from .models import User, Role
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'active_role', 'is_active', 'is_staff')
-    search_fields = ('email', 'username')
-    list_filter = ('is_active', 'is_staff', 'active_role')
-    ordering = ('email',)
-
-@admin.register(Role)
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+# Register your models here.
