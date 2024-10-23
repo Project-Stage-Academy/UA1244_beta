3792ce9b78c5a5d84c9e48e01916f9de9c51c858
Mariana Gogol <marianagogol711@gmail.com>
2024-10-23 20:48:50 +0200

    feat: implement websocket consumers that handle sending and receiving messages in real-time.

--- forum/communications/consumers.py
+++ forum/communications/consumers.py
@@ -1,16 +1,42 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+
+from asgiref.sync import async_to_sync
 import logging
+from channels.generic.websocket import WebsocketConsumer
 
 logger = logging.getLogger(__name__)
 
-
 class CommunicationConsumer(WebsocketConsumer):
     def connect(self):
-        logger.info("WebSocket connection established")
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_group_id = f"chat_{self.room_id}"
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_id, self.channel_name
+        )
+
         self.accept()
 
-        self.send(text_data=json.dumps({
-            "type": "connection_established",
-            "message": "Now you are connected!"
-        }))
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_id, self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_id, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event["message"]
+
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({"message": message}))
